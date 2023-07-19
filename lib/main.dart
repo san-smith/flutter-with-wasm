9c11fbe2e5a7d67bfe92282ed887721ef29e7010
san-smith <san-smith@mail.ru>
2023-07-19 22:50:06 +0700

    prepare project

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,57 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<StatefulWidget> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final int _value = 42;
+  int? _result;
+  bool _inProgress = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$_value-й член ряда Фибоначчи равен: ${_result == null ? '?' : '$_result'}',
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: _onPressed,
+                child: _inProgress
+                    ? const CircularProgressIndicator()
+                    : const Text('Найти'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void _onPressed() {
+    setState(() {
+      _inProgress = true;
+    });
+    _result = fib(_value);
+    setState(() {
+      _inProgress = false;
+    });
+  }
 }
+
+int fib(int n) => switch (n) {
+      < 2 => n,
+      _ => fib(n - 2) + fib(n - 1),
+    };
